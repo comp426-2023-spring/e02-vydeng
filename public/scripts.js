c61d434de0298fbb9af1465cb897f9dcb71e75a6
Vivian Deng <77829789+vydeng@users.noreply.github.com>
2023-05-05 22:44:55 -0400

    started html and js

--- public/scripts.js
+++ public/scripts.js
@@ -1,3 +1,9 @@
 // If you would like to see some examples of similar code to make an interface interact with an API, 
 // check out the coin-server example from a previous COMP 426 semester.
 // https://github.com/jdmar3/coinserver
+let gameForm = document.getElementById("loginForm");
+gameForm.addEventListener("click", onSubmit)
+
+async function onSubmit() {
+    
+}
